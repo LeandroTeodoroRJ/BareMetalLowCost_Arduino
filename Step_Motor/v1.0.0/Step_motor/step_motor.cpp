c9dd553384c018c7a8a52a70e8f07d50849304bd
LeandroTeodoroRJ <leandroteodoro.rj@gmail.com>
2026-08-06 21:06:37 -0300

    Update

---
+++ Step_Motor/v1.0.0/Step_motor/step_motor.cpp
@@ -0,0 +1,57 @@
+#include "Arduino.h"
+#include "step_motor.h"
+#include "gpio.h"
+
+StepMotorDM556::StepMotorDM556(int pulse_pin, int direction_pin){
+  this->pulse.setPin(pulse_pin);
+  this->direction.setPin(direction_pin);
+}
+
+void StepMotorDM556::setEnable(int enable_pin){
+  this->enable.setPin(enable_pin);
+}
+
+void StepMotorDM556::resetDelay(void){
+  this->_delay_to_pulse = 0;
+}
+
+uint16_t StepMotorDM556::getDelay(void){
+  return this->_delay_to_pulse;
+}
+
+void StepMotorDM556::incrementDelay(void){
+  this->_delay_to_pulse++;
+}
+
+void StepMotorDM556::step(void){
+  if(this->_counter_target != this->_actual_counter){
+    if(this->_direction == 0){
+      this->direction.deactivate();
+    }else{
+      this->direction.activate();
+    }
+    this->_pulse = !this->_pulse;
+    if(this->_pulse == 1){
+      this->pulse.activate();
+    }else{
+      this->pulse.deactivate();
+      if (this->_direction == 0){
+        this->_actual_counter--;      
+      }else{
+        this->_actual_counter++;
+      }
+    }
+  }
+}
+
+void StepMotorDM556::setDirection(bool dir){
+  this->_direction = dir;
+}
+
+void StepMotorDM556::setTarget(int16_t target){
+  this->_counter_target = target;
+}
+
+void StepMotorDM556::resetCounter(void){
+  this->_actual_counter = 0;
+}
---
+++ Step_Motor/v1.0.1/Step_motor/Step_motor.ino
@@ -0,0 +1,59 @@
+/*
+ * Project name: Step Motor 
+ * Description: How to use step motor library
+ * Hostpage: https://github.com/LeandroTeodoroRJ/BareMetalLowCost_Arduino
+ * Stable: Yes
+ * Version: 1.1.0
+ * Last Uptate: 06.08.26
+ * Dependences: 
+ *   -- step_motor.h v1.0.1
+ *   -- timer1.h v1.0.0
+ * Current: Yes
+ * Maintainer: leandroteodoro.engenharia@gmail.com
+ * Architecture: Arduino Nano V3
+ * Compile/Interpreter: Arduino IDE Ver 2.3.8
+ * Access: Public
+ * Changelog: No
+ * Readme and Documents: No
+ * Links:
+ *
+ * Other Notes: 
+ *
+ */
+
+#include "step_motor.h"
+#include "timer1.h"
+
+#define GPIO_10   10
+#define GPIO_11   11
+#define DELAY_TIME_MS 10
+
+StepMotorDM556 motor(GPIO_10, GPIO_11);
+
+void setup() {
+  Serial.begin(115200);   
+  noInterrupts(); // Disable global interrupts during configuration
+  configure_1ms_timer1_interrupt();    
+  interrupts();  // Re-enable global interrupts  
+  motor.setDirection(CLOCKWISE);
+  motor.setTarget(400);
+  delay(3000);
+  motor.setDirection(ANTICLOCKWISE);
+  motor.setTarget(-200);
+}
+
+// Timer 1 Interrupt Service Routine (executed exactly every 100 us)
+ISR(TIMER1_COMPA_vect) {  
+  if (DELAY_TIME_MS >= motor.getDelay()){
+    motor.incrementDelay(); //Wait delay time for motor step velocity
+  }else{
+    motor.resetDelay();
+    motor.step();
+  }
+}
+
+void loop() {
+  delay(5000); 
+//  motor.resetCounter();   // Reset to motor move again
+}
+
